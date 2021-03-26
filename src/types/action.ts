3b4f3a7a73e1fb26de1c2f854bf6e68659dd55a8
Rody <rodylorenzowork@gmail.com>
2021-03-26 22:39:11 +0100

    Added types for actions and places

---
+++ src/types/action.ts
@@ -0,0 +1,53 @@
+import { Place } from './places';
+import { User } from './user';
+
+export enum ActionType {
+  LOGIN_REQUEST = 'LOGIN_REQUEST',
+  LOGIN_SUCCESS = 'LOGIN_SUCCESS',
+  LOGIN_FAILURE = 'LOGIN_FAILURE',
+
+  PLACES_REQUEST = 'PLACES_REQUEST',
+  PLACES_SUCCESS = 'PLACES_SUCCESS',
+  PLACES_FAILURE = 'PLACES_FAILURE',
+
+  PLACE_REQUEST = 'PLACE_REQUEST',
+  PLACE_SUCCESS = 'PLACE_SUCCESS',
+  PLACE_FAILURE = 'PLACE_FAILURE'
+}
+
+export type loginRequestType = {
+  type: 'LOGIN_REQUEST'
+}
+export type loginSuccessType = {
+  type: 'LOGIN_SUCCESS',
+  payload: User
+}
+export type loginFailureType = {
+  type: 'LOGIN_FAILURE',
+  payload: string | undefined
+}
+
+export type placesRequestType = {
+  type: 'PLACES_REQUEST'
+}
+export type placesSuccessType = {
+  type: 'PLACES_SUCCESS',
+  payload: Place[]
+}
+export type placesFailureType = {
+  type: 'PLACES_FAILURE',
+  payload: string | undefined
+}
+
+export type placeAddRequestType = {
+  type: "PLACE_ADD_REQUEST",
+  payload: Place
+}
+export type placeAddSuccessType = {
+  type: "PLACE_ADD_SUCCESS",
+  payload: Place
+}
+export type placeAddFailureType = {
+  type: "PLACE_ADD_FAILURE",
+  payload: string | undefined
+}
---
+++ src/types/places.ts
@@ -0,0 +1,14 @@
+export interface Place {
+  name: string;
+  lat: number;
+  lon: number;
+  description: string;
+  phone: string;
+  zip: number;
+  address: string;
+  cap: number;
+  city: string;
+  province: string;
+  categories: number;
+  extra?: string;
+}
