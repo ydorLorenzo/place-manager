2022fd52e6fe9b7041db5a1bd545806456dce35e
Rody <rodylorenzowork@gmail.com>
2021-03-29 12:26:09 +0200

    Completed form binding and create action dispatched.

--- src/components/pages/places/CreatePlace.tsx
+++ src/components/pages/places/CreatePlace.tsx
@@ -1,5 +1,5 @@
-import React, { FormEvent, ReactElement, useState } from 'react';
-import { Button, Card, Form, FormControl, FormControlProps, FormProps } from 'react-bootstrap';
+import React, { FormEvent, ReactElement } from 'react';
+import { Button, Card, Form, FormProps } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { addPlaceAction } from '../../../redux/actions/place';
 import { Place } from '../../../types/places';
@@ -10,13 +10,18 @@ interface PlaceProps extends FormProps {
 }
 
 function PlaceForm (props: PlaceProps): ReactElement {
-  const [validated, setValidated] = useState(false);
 
   const name = useFormInput('');
-  const description = useFormInput('')
-  const province = useFormInput('')
-  const address = useFormInput('')
-  const city = useFormInput('')
+  const description = useFormInput('');
+  const province = useFormInput('');
+  const address = useFormInput('');
+  const city = useFormInput('');
+  const categories = useFormInput([1]);
+  const zip = useFormInput('');
+  const lon = useFormInput(0);
+  const lat = useFormInput(0);
+  const phone = useFormInput('');
+  const cap = useFormInput('');
 
   const CATEGORY_OPTIONS = [
     'Ristorante',
@@ -24,17 +29,20 @@ function PlaceForm (props: PlaceProps): ReactElement {
     'Pescheria'
   ];
 
-  const handleSubmit = (event: React.FormEvent<typeof FormControl & FormControlProps>) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     const form = event.currentTarget;
+    event.preventDefault();
     props.addPlace({
-      name, description, province, address, city
-    })
-  }
+      name: name.value, description: description.value, province: province.value,
+      address: address.value, city: city.value, categories: [categories.value], zip: zip.value,
+      lon: lon.value, lat: lat.value, cap: cap.value, phone: phone.value
+    });
+  };
 
   return (
     <Card>
       <Card.Body>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Row>
             <Form.Group className="col-md-8">
               <Form.Label>Name</Form.Label>
@@ -42,8 +50,8 @@ function PlaceForm (props: PlaceProps): ReactElement {
             </Form.Group>
             <Form.Group className="col-md-4">
               <Form.Label>Categories</Form.Label>
-              <Form.Control as="select">
-                {CATEGORY_OPTIONS.map((value,key) => (<option key={key} value={key}>{value}</option>))}
+              <Form.Control {...categories} as="select" multiple>
+                {CATEGORY_OPTIONS.map((value, key) => (<option key={key} value={key + 1}>{value}</option>))}
               </Form.Control>
             </Form.Group>
             <Form.Group className="col-md-12">
@@ -64,23 +72,23 @@ function PlaceForm (props: PlaceProps): ReactElement {
             </Form.Group>
             <Form.Group className="col-md-4">
               <Form.Label>Zip</Form.Label>
-              <input pattern={'^[0-9]{5}$'} className="form-control"/>
+              <input pattern={'^[0-9]{5}$'} {...zip} className="form-control"/>
             </Form.Group>
             <Form.Group className="col-md-3">
               <Form.Label>Longitude</Form.Label>
-              <input type="number" step={.1} min={-180} max={180} className="form-control" />
+              <input {...lon} type="number" step={.0000001} min={-180} max={180} className="form-control"/>
             </Form.Group>
             <Form.Group className="col-md-3">
               <Form.Label>Latitude</Form.Label>
-              <input type="number" step={.1} min={-90} max={90} className="form-control" />
+              <input {...lat} type="number" step={.0000001} min={-90} max={90} className="form-control"/>
             </Form.Group>
             <Form.Group className="col-md-3">
               <Form.Label>Phone</Form.Label>
-              <input pattern={'^(([+]|00)39\\s{0,1})?(([0-6][0-9]))([0-9]{8})$'} className="form-control"/>
+              <input {...phone} pattern={'^(([+]|00)39\\s{0,1})?(([0-6][0-9]))([0-9]{8})$'} className="form-control"/>
             </Form.Group>
             <Form.Group className="col-md-3">
               <Form.Label>Cap</Form.Label>
-              <input pattern={'^[0-9]{5}$'} className="form-control"/>
+              <input {...cap} pattern={'^[0-9]{5}$'} className="form-control"/>
             </Form.Group>
           </Form.Row>
           <Button type="submit" className="col-lg-2 offset-lg-5 col-sm-4 offset-sm-4 w-100">
