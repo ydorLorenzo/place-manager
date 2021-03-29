d9b5d4b84fd8f0b479fbec13e515d771fc55dd49
Rody <rodylorenzowork@gmail.com>
2021-03-29 02:10:30 +0200

    Update api service to login and intercept requests and responses with axios.

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,6 +1,6 @@
-import axios  from 'axios';
+import axios from 'axios';
 
-export const apiBaseRoute = "http://144.91.111.4:3005/api";
+export const apiBaseRoute = 'http://144.91.111.4:3005/api';
 
 export enum MethodType {
   POST = 'POST',
@@ -20,11 +20,60 @@ export interface apiProp<T> {
   auth: boolean;
 }
 
-export function fetchApiCall(url='', method=MethodType.GET, data={}, auth=false): Promise<any> {
-  const headers: HeadersInit = {
-    "Content-Type": "application/json"
+const instance = axios.create();
+
+export function loginApiCall (email: string, password: string): Promise<any> {
+  return instance.post(
+    apiBaseRoute + loginRoute,
+    JSON.stringify({ password, email }),
+    { headers: { 'Content-Type': 'application/json' } })
+    .then(response => {
+      console.log(response);
+      return response
+    })
+    .catch(error => {
+      if (error.response) {
+        console.log('Error in the server');
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log('Error in the request');
+        console.log(error.request);
+      } else {
+        console.log('Error setting up the request');
+        console.log('Error: ', error.message);
+      }
+      console.log(error.config);
+    });
+}
+
+axios.interceptors.request.use(
+  (axiosRequest) => {
+    const token = localStorage.getItem('token');
+    if (!!token) {
+      axiosRequest.headers['Authorization'] = 'Bearer ' + JSON.parse(token);
+    }
+    return axiosRequest;
+  },
+  error => Promise.reject(error)
+);
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status === 401) {
+      console.log('finding this error: ', error);
+    }
+    return Promise.reject(error)
   }
-  url = apiBaseRoute+url;
+);
+
+export function fetchApiCall (url = '', method = MethodType.GET, data = {}, auth = false): Promise<any> {
+  const headers: HeadersInit = {
+    'Content-Type': 'application/json'
+  };
+  url = apiBaseRoute + url;
   return axios.request({
     method,
     headers,
