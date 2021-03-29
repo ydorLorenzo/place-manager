24dec6781661a2294d27f18396e7dd785d845d59
Rody <rodylorenzowork@gmail.com>
2021-03-29 02:09:35 +0200

    Added dev tools for chrome debugging.

--- src/redux/sagas/placesSaga.ts
+++ src/redux/sagas/placesSaga.ts
@@ -1,33 +1,30 @@
-import { put, call, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { CallReturnType } from '../../types/sagas';
 
-import {
-  ActionType, placeRequestType,
-} from '../../types/action';
+import { ActionType, placeRequestType, } from '../../types/action';
 import { fetchApiCall, placeRoute } from '../../services/ApiService';
 
-
-function* fetchPlacesAsync(): Generator {
+function * fetchPlacesAsync (): Generator {
   try {
     const response = yield call(fetchApiCall, placeRoute);
     console.log(response);
-    yield put({type: ActionType.PLACES_SUCCESS, payload: response});
+    yield put({ type: ActionType.PLACES_SUCCESS, payload: response });
   } catch (e) {
-    yield put({ type: ActionType.PLACES_FAILURE, payload: e.message })
+    yield put({ type: ActionType.PLACES_FAILURE, payload: e.message });
   }
 }
 
-function* fetchPlaceAsync(action: placeRequestType): Generator {
+function * fetchPlaceAsync (action: placeRequestType): Generator {
   try {
-    const response: CallReturnType<typeof fetchApiCall> = yield call(fetchApiCall, placeRoute+'/'+action.payload);
+    const response: CallReturnType<typeof fetchApiCall> = yield call(fetchApiCall, placeRoute + '/' + action.payload);
     console.log(response);
-    yield put({type: ActionType.PLACE_SUCCESS, payload: response})
+    yield put({ type: ActionType.PLACE_SUCCESS, payload: response });
   } catch (e) {
-    yield put({type: ActionType.PLACES_FAILURE, payload: e.message})
+    yield put({ type: ActionType.PLACES_FAILURE, payload: e.message });
   }
 }
 
-export default function* placesSaga(): Generator {
+export default function * placesWatcher (): Generator {
   yield takeLatest(ActionType.PLACES_REQUEST, fetchPlacesAsync);
   yield takeLatest(ActionType.PLACE_REQUEST, fetchPlaceAsync);
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
-import rootSagas from './sagas';
 
+import rootReducer from './reducers';
+import rootSagas from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(sagaMiddleware),
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(sagaMiddleware
+  ))
 );
+
 sagaMiddleware.run(rootSagas);
 
 export type RootState = ReturnType<typeof store.getState>;
