72ab158b14461f1a2034c82fa65271c63c1bdca9
Rody <rodylorenzowork@gmail.com>
2021-03-30 09:19:54 +0200

    Adding logic to render proper nav and redirect to proper component in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,74 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { Container, Nav } from 'react-bootstrap';
 import PlaceList from './components/pages/places/Places';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { Link, Redirect, Route, useHistory, useLocation } from 'react-router-dom';
 import LoginBox from './components/pages/login/LoginBox';
 import PlaceForm from './components/pages/places/CreatePlace';
+import { logoutAction } from './redux/actions/auth';
+import { useSelector } from 'react-redux';
+import { RootState } from './redux/store';
 import { useAppDispatch } from './types/hooks';
-import { userUnset } from './redux/actions/user';
 
 export default function App (): ReactElement {
+
+  const { authenticated } = useSelector((state: RootState) => state.auth);
+  const [token, setToken] = useState<string | null>(null);
+  const history = useHistory();
+  const pathname = usePathname();
   const dispatch = useAppDispatch();
 
-  const token = localStorage.getItem('token');
+  useEffect(() => {
+    setToken(localStorage.getItem('token'));
+    if (!token || !authenticated) history.push('/login');
+    if (authenticated && pathname === '/login') {
+      history.push('/places');
+    }
+  }, [authenticated]);
 
-  const logout = () => {
-    return dispatch(userUnset());
+  const onLogout = (e: any) => {
+    e.preventDefault();
+    dispatch(logoutAction());
   };
+
   return (
-    <BrowserRouter>
-      <Container className="mt-5">
-        <Nav className="justify-content-end">
+    <Container className="mt-5">
+      <Nav className="justify-content-end">
+        <Nav.Item>
+          <Link to="/places" className='nav-link'>
+            Home
+          </Link>
+        </Nav.Item>
+        {
+          (!!token || authenticated) &&
           <Nav.Item>
-            <Nav.Link href="/places" className={location.pathname === '/places' ? 'text-black-50' : ''}>Home</Nav.Link>
+            <Link to="/places/add" className='nav-link'>Add
+              place</Link>
           </Nav.Item>
-          {
-            !!token &&
-            <Nav.Item>
-              <Nav.Link href="/places/add" className={location.pathname === '/places/add' ? 'text-black-50' : ''}>Add
-                place</Nav.Link>
-            </Nav.Item>
-          }
-          {
-            !token &&
-            <Nav.Item>
-              <Nav.Link href="/login" className={location.pathname === '/login' ? 'text-black-50' : ''}>Login</Nav.Link>
-            </Nav.Item>
-          }
-          {
-            !!token &&
-            <Nav.Item>
-              <a className="nav-link" href="#" onClick={logout}>Logout</a>
-            </Nav.Item>
-          }
-        </Nav>
-        <Container>
-          <Route path="/" exact render={() => (<Redirect to="/places"/>)}/>
-          <Route path="/login" component={LoginBox}/>
-          <Route path="/places" exact component={PlaceList}/>
-          <Route path="/places/add" component={PlaceForm}/>
-        </Container>
+        }
+        {
+          !token &&
+          <Nav.Item>
+            <Link to="/login" className='nav-link'>Login</Link>
+          </Nav.Item>
+        }
+        {
+          (!!token || authenticated) &&
+          <Nav.Item>
+            <a href="" className="nav-link" onClick={onLogout}>Logout</a>
+          </Nav.Item>
+        }
+      </Nav>
+      <Container>
+        <Route path="/" exact render={() => (<Redirect to="/places"/>)}/>
+        <Route path="/login" component={LoginBox}/>
+        <Route path="/places" exact component={PlaceList}/>
+        <Route path="/places/add" component={PlaceForm}/>
       </Container>
-    </BrowserRouter>
+    </Container>
   );
+}
+
+function usePathname() {
+  const location = useLocation();
+  return location.pathname;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 
 import store from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
+    <BrowserRouter>
     <App/>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
